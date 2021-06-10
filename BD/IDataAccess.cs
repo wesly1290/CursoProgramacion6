947b56e5a5adaf4cd05779e46e9319c69d15f43f
HP <HP@DESKTOP-OSB4OO7>
2021-06-10 03:09:50 -0600

    Add project files.

---
+++ BD/IDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BD
+{
+    public interface IDataAccess
+    {
+        Task<DBEntity> ExecuteAsync(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<dynamic>> QueryAsync(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<T>> QueryAsync<T, B, C, D, E, F, G>(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<T>> QueryAsync<T, B, C, D, E, F>(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<T>> QueryAsync<T, B, C, D, E>(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<T>> QueryAsync<T, B, C, D>(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<T>> QueryAsync<T, B, C>(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<T>> QueryAsync<T, B>(string sp, object Param = null, int? Timeout = null);
+        Task<IEnumerable<T>> QueryAsync<T>(string sp, object Param = null, int? Timeout = null);
+        Task<T> QueryFisrtAsync<T>(string sp, object Param = null, int? Timeout = null);
+    }
+}
---
+++ Entity/DBEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+     public class DBEntity
+    {
+        public int CodeError { get; set; }
+        public string MsgError { get; set; }
+
+    }
+}
