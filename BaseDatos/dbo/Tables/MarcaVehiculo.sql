e84f05c2db7a473f8d38e6528d30e1dfab19ead3
WESLY DANIEL SOLANO <wsolanor0724@uvirtual.ac.cr>
2021-06-10 21:40:16 -0600

    Actualización de bd

---
+++ BaseDatos/dbo/Tables/MarcaVehiculo.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[MarcaVehiculo]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ BaseDatos/dbo/Tables/Vehiculo.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Vehiculo]
+(
+	VehiculoId INT NOT NULL IDENTITY (1,1) CONSTRAINT PK_Vehiculo PRIMARY KEY CLUSTERED(VehiculoId)
+	, Matricula VARCHAR(250) NOT NULL
+	, Color VARCHAR(250) NOT NULL
+	, Modelo VARCHAR(250) NOT NULL
+	, FechaModelo DATE NOT NULL CONSTRAINT DF_Vehiculo_FechaModelo default('2020-01-01')
+	, Estado BIT NOT NULL
+)
+WITH (DATA_COMPRESSION = PAGE)
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX IDX_Vehiculo_Matricula
+ON dbo.Vehiculo(Matricula)
+WITH (DATA_COMPRESSION = PAGE)
+GO
